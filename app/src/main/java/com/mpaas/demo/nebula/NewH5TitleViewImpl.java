82ffdd13501d323251d1ec3fd42bf22fde57a920
重岳 <rocky.lqp@antfin.com>
2020-06-18 12:35:31 +0800

    update custom title example

--- app/src/main/java/com/mpaas/demo/nebula/NewH5TitleViewImpl.java
+++ app/src/main/java/com/mpaas/demo/nebula/NewH5TitleViewImpl.java
@@ -3,6 +3,7 @@ package com.mpaas.demo.nebula;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import com.alipay.mobile.h5container.api.H5ImageListener;
 import com.alipay.mobile.nebula.util.H5ImageUtil;
 import com.alipay.mobile.nebula.util.H5StatusBarUtils;
+import com.alipay.mobile.nebula.util.H5Utils;
 import com.alipay.mobile.nebula.view.AbsTitleView;
 import com.alipay.mobile.nebula.view.H5TitleBarFrameLayout;
 import com.mpaas.demo.R;
@@ -30,11 +32,15 @@ public class NewH5TitleViewImpl extends AbsTitleView {
 
     private View btnBack;
 
-    private View optionContainer;
+    private View options;
+    private ImageView oImage;
+    private TextView oTv;
 
     private View options1;
+    private ImageView o1Image;
+    private TextView o1Tv;
 
-    private View btHome;
+    private View btClose;
 
     private Context context;
 
@@ -57,14 +63,22 @@ public class NewH5TitleViewImpl extends AbsTitleView {
                 invokePageBackEvent();
             }
         });
-        optionContainer = content.findViewById(R.id.options);
-        btHome = content.findViewById(R.id.home);
-        int statusBarHeight = H5StatusBarUtils.getStatusBarHeight(context);
-        content.setPadding(0, statusBarHeight, 0, 0);
-        btHome.setOnClickListener(new View.OnClickListener() {
+        options = content.findViewById(R.id.options);
+        options.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                invokeHomeClickEvent();
+                invokeOptionClickEvent(0, false);
+            }
+        });
+        oImage = options.findViewById(R.id.oimage);
+        oTv = options.findViewById(R.id.otext);
+        btClose = content.findViewById(R.id.home);
+        int statusBarHeight = H5StatusBarUtils.getStatusBarHeight(context);
+        content.setPadding(0, statusBarHeight, 0, 0);
+        btClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                invokePageCloseEvent();
             }
         });
         options1 = content.findViewById(R.id.options1);
@@ -74,6 +88,8 @@ public class NewH5TitleViewImpl extends AbsTitleView {
                 invokeOptionClickEvent(1, false);
             }
         });
+        o1Image = options1.findViewById(R.id.o1image);
+        o1Tv = options1.findViewById(R.id.o1text);
     }
 
     @Override
@@ -129,7 +145,7 @@ public class NewH5TitleViewImpl extends AbsTitleView {
 
     @Override
     public void showCloseButton(boolean b) {
-
+        btClose.setVisibility(b ? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -144,13 +160,14 @@ public class NewH5TitleViewImpl extends AbsTitleView {
 
     @Override
     public void showBackHome(boolean b) {
-        btHome.setVisibility(b ? View.VISIBLE : View.GONE);
+        btClose.setVisibility(b ? View.VISIBLE : View.GONE);
     }
 
 
     @Override
     public void showOptionMenu(boolean b) {
         options1.setVisibility(b ? View.VISIBLE : View.GONE);
+        options.setVisibility(b ? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -158,20 +175,62 @@ public class NewH5TitleViewImpl extends AbsTitleView {
         if (i == 1) {
             return options1;
         }
-        return optionContainer;
+        return options;
     }
 
     @Override
     public void setOptionMenu(boolean reset, boolean override, boolean isTinyApp, List<MenuData> menus) {
+        if (reset) {
+            // reset status
+            options.setVisibility(View.GONE);
+            options1.setVisibility(View.GONE);
+        }
         for (int i = 0; i < 2 && i < menus.size(); i++) {
             MenuData menuData = menus.get(i);
             if (isTinyApp) {
-                String iconUrl = menuData.getIcon();
-                if (!TextUtils.isEmpty(iconUrl)) {
-                    H5ImageUtil.loadImage(iconUrl, new H5ImageListener() {
+                // ignore
+            } else {
+                if (!TextUtils.isEmpty(menuData.getTitle())) {
+                    try {
+                        if (i == 0) {
+                            oTv.setVisibility(View.VISIBLE);
+                            oImage.setVisibility(View.GONE);
+                            oTv.setText(menuData.getTitle());
+                            oTv.setTextColor(Color.parseColor(menuData.getColor()));
+                        } else {
+                            o1Tv.setVisibility(View.VISIBLE);
+                            o1Image.setVisibility(View.GONE);
+                            o1Tv.setText(menuData.getTitle());
+                            o1Tv.setTextColor(Color.parseColor(menuData.getColor()));
+                        }
+                    } catch (Exception e) {
+                        //
+                    }
+                } else if (!TextUtils.isEmpty(menuData.getIconType())) {
+                    // choose right image with different icon type
+                    if (i == 0) {
+                        oTv.setVisibility(View.GONE);
+                        oImage.setVisibility(View.VISIBLE);
+                        oImage.setImageResource(R.drawable.smile);
+                    } else {
+                        o1Tv.setVisibility(View.GONE);
+                        o1Image.setVisibility(View.VISIBLE);
+                        o1Image.setImageResource(R.drawable.smile);
+                    }
+                } else if (!TextUtils.isEmpty(menuData.getIcon())) {
+                    final int finalI = i;
+                    H5ImageUtil.loadImage(menuData.getIcon(), new H5ImageListener() {
                         @Override
                         public void onImage(Bitmap bitmap) {
-                            ((ImageView) options1.findViewById(R.id.o1image)).setImageBitmap(bitmap);
+                            if (finalI == 0) {
+                                oTv.setVisibility(View.GONE);
+                                oImage.setVisibility(View.VISIBLE);
+                                oImage.setImageBitmap(bitmap);
+                            } else {
+                                o1Tv.setVisibility(View.GONE);
+                                o1Image.setVisibility(View.VISIBLE);
+                                o1Image.setImageBitmap(bitmap);
+                            }
                         }
                     });
                 }
@@ -187,6 +246,6 @@ public class NewH5TitleViewImpl extends AbsTitleView {
 
     @Override
     public View getPopAnchor() {
-        return optionContainer;
+        return options;
     }
 }
